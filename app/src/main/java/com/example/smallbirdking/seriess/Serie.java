ba23a1c89864d2ebbf9d55d71bed06ce9d605c1e
smallbirdking <869084711@qq.com>
2015-10-15 23:15:24 +0200

    photo add

--- app/src/main/java/com/example/smallbirdking/seriess/Serie.java
+++ app/src/main/java/com/example/smallbirdking/seriess/Serie.java
@@ -1,16 +1,36 @@
 package com.example.smallbirdking.seriess;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.smallbirdking.seriess.function.response;
 import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 /**
  * Created by smallbirdking on 2015/10/14.
@@ -18,30 +38,42 @@ import com.squareup.picasso.Picasso;
 public class Serie extends AppCompatActivity {
     public static final String S_ITEM_ID = "item_id";
     private response.ResultsEntity mItem;
+
+    private GridView gridView1;                   //网格显示缩略图
+    private Button buttonPublish;                //发布按钮
+    private final int IMAGE_OPEN = 1;        //打开图片标记
+    private String pathImage;                       //选择图片路径
+    private Bitmap bmp;                               //导入临时图片
+    private ArrayList<HashMap<String, Object>> imageItem;
+    private SimpleAdapter simpleAdapter;     //适配器
+    Button btnTakePhoto;
+    private static final int CAM_REQUEST = 1313;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.i("serie", "start");
+
         super.onCreate(savedInstanceState);
+        Log.i("argu", "start000");
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.
+                SOFT_INPUT_ADJUST_PAN);
+        //锁定屏幕
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_serie_detail);
-
+        Log.i("argu", "start00");
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
-
+        Log.i("argu", "start0");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-
+        Log.i("argu", "start");
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
+
             Bundle arguments = new Bundle();
             arguments.putString(S_ITEM_ID,
                     getIntent().getStringExtra(S_ITEM_ID));
             Log.i("argu", arguments.getString(S_ITEM_ID));
-            /*ItemDetailFragment fragment = new ItemDetailFragment(Serie.this);
-            fragment.setArguments(arguments);
 
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.serie_detail_container, fragment)
-                    .commit();*/
 
             if (arguments.containsKey(S_ITEM_ID)) {
                 // Load the dummy content specified by the fragment
@@ -55,7 +87,7 @@ public class Serie extends AppCompatActivity {
                     appBarLayout.setTitle(mItem.getName());
                 }
             }
-
+            Log.i("argu", "end");
 
             if (mItem != null) {
                 String imageURL = content.urlImage+mItem.getPoster_path();
@@ -65,8 +97,194 @@ public class Serie extends AppCompatActivity {
             }
         }
 
+        Log.i("gallery","Start");
+        //获取控件对象
+        gridView1 = (GridView) findViewById(R.id.gridView1);
+
+        /*
+         * 载入默认图片添加图片加号
+         * 通过适配器实现
+         * SimpleAdapter参数imageItem为数据源 R.layout.griditem_addpic为布局
+         */
+        bmp = BitmapFactory.decodeResource(getResources(), R.drawable.gridview_addpic); //加号
+        imageItem = new ArrayList<HashMap<String, Object>>();
+        HashMap<String, Object> map = new HashMap<String, Object>();
+        map.put("itemImage", bmp);
+        imageItem.add(map);
+        simpleAdapter = new SimpleAdapter(this,
+                imageItem, R.layout.griditem_addpic,
+                new String[] { "itemImage"}, new int[] { R.id.imageView1});
+        /*
+         * HashMap载入bmp图片在GridView中不显示,但是如果载入资源ID能显示 如
+         * map.put("itemImage", R.drawable.img);
+         * 解决方法:
+         *              1.自定义继承BaseAdapter实现
+         *              2.ViewBinder()接口实现
+         *  参考 http://blog.csdn.net/admin_/article/details/7257901
+         */
+        simpleAdapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+            @Override
+            public boolean setViewValue(View view, Object data,
+                                        String textRepresentation) {
+                // TODO Auto-generated method stub
+                if (view instanceof ImageView && data instanceof Bitmap) {
+                    ImageView i = (ImageView) view;
+                    i.setImageBitmap((Bitmap) data);
+                    return true;
+                }
+                return false;
+            }
+        });
+        gridView1.setAdapter(simpleAdapter);
+        Log.i("gallery", "click");
+        /*
+         * 监听GridView点击事件
+         * 报错:该函数必须抽象方法 故需要手动导入import android.view.View;
+         */
+        gridView1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+            {
+                if( imageItem.size() == 10) { //第一张为默认图片
+                    Toast.makeText(Serie.this, "图片数9张已满", Toast.LENGTH_SHORT).show();
+                }
+                else if(position == 0) { //点击图片位置为+ 0对应0张图片
+                    Toast.makeText(Serie.this, "添加图片", Toast.LENGTH_SHORT).show();
+                    //选择图片
+                    Intent intent = new Intent(Intent.ACTION_PICK,
+                            android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, IMAGE_OPEN);
+                    //通过onResume()刷新数据
+                }
+                else {
+                    dialog(position);
+                    //Toast.makeText(MainActivity.this, "点击第" + (position + 1) + " 号图片",
+                    //		Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
 
+
+        btnTakePhoto = (Button) findViewById(R.id.button_photo);
+
+        btnTakePhoto.setOnClickListener(new btnTakePhotoCliker());
+        Log.i("gallery", "end");
+    }
+    class btnTakePhotoCliker implements Button.OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+            Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(cameraintent,CAM_REQUEST);
+        }
     }
 
+    //获取图片路径 响应startActivityForResult
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == CAM_REQUEST){
+            Bitmap thumnail = (Bitmap)  data.getExtras().get("data");
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("itemImage", thumnail);
+            imageItem.add(map);
+            simpleAdapter = new SimpleAdapter(this,
+                    imageItem, R.layout.griditem_addpic,
+                    new String[] { "itemImage"}, new int[] { R.id.imageView1});
+            simpleAdapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+                @Override
+                public boolean setViewValue(View view, Object data,
+                                            String textRepresentation) {
+                    // TODO Auto-generated method stub
+                    if(view instanceof ImageView && data instanceof Bitmap){
+                        ImageView i = (ImageView)view;
+                        i.setImageBitmap((Bitmap) data);
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            gridView1.setAdapter(simpleAdapter);
+            simpleAdapter.notifyDataSetChanged();
+        }
+        //打开图片
+        if(resultCode==RESULT_OK && requestCode==IMAGE_OPEN) {
+            Uri uri = data.getData();
+            if (!TextUtils.isEmpty(uri.getAuthority())) {
+                //查询选择图片
+                Cursor cursor = getContentResolver().query(
+                        uri,
+                        new String[] { MediaStore.Images.Media.DATA },
+                        null,
+                        null,
+                        null);
+                //返回 没找到选择图片
+                if (null == cursor) {
+                    return;
+                }
+                //光标移动至开头 获取图片路径
+                cursor.moveToFirst();
+                pathImage = cursor.getString(cursor
+                        .getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+        }  //end if 打开图片
+    }
+
+    //刷新图片
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!TextUtils.isEmpty(pathImage)){
+            Bitmap addbmp=BitmapFactory.decodeFile(pathImage);
+            HashMap<String, Object> map = new HashMap<String, Object>();
+            map.put("itemImage", addbmp);
+            imageItem.add(map);
+            simpleAdapter = new SimpleAdapter(this,
+                    imageItem, R.layout.griditem_addpic,
+                    new String[] { "itemImage"}, new int[] { R.id.imageView1});
+            simpleAdapter.setViewBinder(new SimpleAdapter.ViewBinder() {
+                @Override
+                public boolean setViewValue(View view, Object data,
+                                            String textRepresentation) {
+                    // TODO Auto-generated method stub
+                    if(view instanceof ImageView && data instanceof Bitmap){
+                        ImageView i = (ImageView)view;
+                        i.setImageBitmap((Bitmap) data);
+                        return true;
+                    }
+                    return false;
+                }
+            });
+            gridView1.setAdapter(simpleAdapter);
+            simpleAdapter.notifyDataSetChanged();
+            //刷新后释放防止手机休眠后自动添加
+            pathImage = null;
+        }
+    }
+
+    /*
+     * Dialog对话框提示用户删除操作
+     * position为删除图片位置
+     */
+    protected void dialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(Serie.this);
+        builder.setMessage("确认移除已添加图片吗？");
+        builder.setTitle("提示");
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+                imageItem.remove(position);
+                simpleAdapter.notifyDataSetChanged();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
 }
